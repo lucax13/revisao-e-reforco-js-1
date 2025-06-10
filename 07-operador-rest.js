b0562c56b0f6c478a919942d52b2a37cfe766eb6
lucax13 <lucas13lima2006@gmail.com>
2025-06-10 11:25:59 -0300

    .

--- 07-operador-rest.js
+++ 07-operador-rest.js
@@ -1,4 +1,7 @@
 'use strict';
+import { classificar } from "./modulos-esm/funcoes.js";
+
+
 /*sobre o operador rest ...
 usmos o rest para mesclar uma relação de paramentros/argumentos
 para uma função.*/ 
@@ -18,4 +21,13 @@ function somar(...valores){
 let resultado1 = somar(10,20);
 let resultado2 = somar(10, 20, 50);
 console.log(resultado1);
-console.log(resultado2);    
+console.log(resultado2);   
+
+console.log("=-=-=-=-=-=-=-=-=-=-=-=-");
+
+console.log(classificar ("limão", "mamão", "maracuja", "laranja"));
+console.log(
+    classificar(
+        "yes", "rush", "pink floy", "iron maiden", "slayer"
+    )
+);
---
+++ 08-operador-spread.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*sobe o operador spread ... 
+usamos o spread para "espalhar" dados de arrays e objetos,
+o que pode ser útil na chamada de funções e também na mesclagem destas estruturas.*/
+
+// spread em arrays
+const bandas = ["yes", "rush", "criolo"];
+const maisBandas = ["dream theater", "savage", ...bandas];
+
+console.log(bandas);
+console.log(maisBandas);
