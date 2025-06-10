76da3494932965423eb4138c7711deb6851bb5bf
lucax13 <lucas13lima2006@gmail.com>
2025-06-10 11:29:44 -0300

    Update 08-operador-spread.js

--- 08-operador-spread.js
+++ 08-operador-spread.js
@@ -1,5 +1,8 @@
 'use strict';
 
+import { separador } from "./modulos-esm/exemplos.js";
+import { classificar } from "./modulos-esm/funcoes.js";
+
 /*sobe o operador spread ... 
 usamos o spread para "espalhar" dados de arrays e objetos,
 o que pode ser útil na chamada de funções e também na mesclagem destas estruturas.*/
@@ -10,3 +13,7 @@ const maisBandas = ["dream theater", "savage", ...bandas];
 
 console.log(bandas);
 console.log(maisBandas);
+
+separador();
+
+console.log( classificar(...maisBandas));
