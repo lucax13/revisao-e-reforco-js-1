c298385f22bb179633758a5db3af191ed244d546
lucax13 <lucas13lima2006@gmail.com>
2025-06-11 08:47:25 -0300

    Create 09-desestruturacao.js

---
+++ 09-desestruturacao.js
@@ -0,0 +1,28 @@
+"use strict";
+// 09-desestruturacao.js
+
+import { separador } from "./modulos-esm/exemplos.js";
+
+// Desestruturando arrays
+const clientes = ["Jon Oliva", "Geddy Lee", "David Gilmour"];
+const [cliente1, cliente2, cliente3] = clientes;
+
+console.log(cliente1);
+console.log(cliente2);
+console.log(cliente3);
+
+separador();
+
+const capitais = [
+  "João Pessoa",
+  "Manaus",
+  "Curitiba",
+  "Salvador",
+  "Belo Horizonte",
+];
+
+const [paraiba, amazonas, parana, bahia, minasGerais] = capitais;
+
+console.log(paraiba);
+console.log(amazonas);
+console.log(bahia);
