e211bbf77fab2c96bfb8d0c833d4ec7e0382fbc4
lucax13 <lucas13lima2006@gmail.com>
2025-06-11 09:04:00 -0300

    desestruturação alinhada

--- 09-desestruturacao.js
+++ 09-desestruturacao.js
@@ -43,3 +43,10 @@ const [frontEnd, backEnd] = linguagens;
 
 console.log(frontEnd[2]);
 console.log(backEnd[3]);
+
+separador();
+
+//desestruturação alinhada
+const [[html, css, js], [php, node, sql, python, asp]] = linguagens;
+console.log(js);
+console.log(python);
