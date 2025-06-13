ac2b1cf3420102971c444329e5dd3ab0454d599f
lucax13 <lucas13lima2006@gmail.com>
2025-06-13 09:52:17 -0300

    Update 11-filter.js

--- 11-filter.js
+++ 11-filter.js
@@ -13,3 +13,19 @@ const META = 1000;
 const vendasQueBateramMeta = vendas.filter((venda) => venda >= META);
 
 console.log(vendasQueBateramMeta);
+
+separador();
+
+//exmplo 2: filter serviçõs pendentes
+const servcos = [
+  { codigo: 1, tipo: "limpeza", status: "concluido" },
+  { codigo: 2, tipo: "manutenção", status: "pendente" },
+  { codigo: 3, tipo: "instalação", status: "concluido" },
+  { codigo: 4, tipo: "reparo", status: "pendente" },
+  { codigo: 5, tipo: "consultoria", status: "concluido" },
+];
+
+const servicoPendente = servcos.filter((servico) => {
+  return servico.status == "pendente";
+});
+console.log(servicoPendente);
