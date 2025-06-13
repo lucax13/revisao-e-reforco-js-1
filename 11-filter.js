9548ef62ef1d9d6be60e380b80b43c5057e60749
lucax13 <lucas13lima2006@gmail.com>
2025-06-13 09:21:19 -0300

    Create 11-filter.js

---
+++ 11-filter.js
@@ -0,0 +1,17 @@
+"use strict";
+//11-filter.js
+
+import cursos from "./modulos-esm/cursos.js";
+import { separador } from "./modulos-esm/exemplos.js";
+
+/*filter (filtrar) passa por elementos de um array e executa uma função callback
+visando reornar valores de acordo com uma ou mais condições, gerando um novo array.*/
+
+const vendas = [1500, 200, 905, 3000, 780, 9000];
+const META = 1000;
+
+const vendasQueBateramMeta = vendas.filter((venda) => {
+  return venda >= META;
+});
+
+console.log(vendasQueBateramMeta);
